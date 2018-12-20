a6214a66a9e9b473eaf5f0a2d0ea82293b784dc1
itwill <itwill@ITWILLBS04-30>
2018-12-20 15:20:55 +0900

    2018-12-20
    회원+메시지 중간정도

--- WebContent/message/sendMessage.jsp
+++ WebContent/message/sendMessage.jsp
@@ -9,29 +9,32 @@
 
 </head>
 <body>
-	<!-- 세션id값 전달받기  -->
-	<% String id=(String)session.getAttribute("id"); %>
+<!-- 세션id값 전달받기  -->
+<% String id=(String)session.getAttribute("id"); %>
 
+<section class="messageSend_sec"><!-- TOP과 FOOTER 사이를 차지하는 공간. -->
+
+<div class="send_div">
 <!-- 메시지 작성을 마치고 Send버튼을 눌렀을 시 실제 메시지 작업을하는  SendMessageAction.java로 -->
 <form action="./SendMessageAction.message" method="post">
-<fieldset>
-	<label>보내는 사람 ID : </label> 
-	<!-- 보내는사람은 접속한 사용자ID로 고정 -->
-	<input type="text" name="toID" value="<%=id%>" readonly>
+<!-- 보내는 사람은 사용자 본인 고정 -->
+<input type="hidden" name="toID" value="<%=id%>"> 
+
+<fieldset class="send_FS"> 
 	<label>받는사람 ID : </label>
 	<input type="text" name="fromID">
 	<label>제목 : </label>
 	<input type="text" name="subject">
 	<br/>
-	<label>내용</label><br/>
-	<textarea rows="10" cols="100" name="content"></textarea>
-</fieldset>
-
-<fieldset>
+	<!-- 내용 -->
+	<textarea name="content" class="send_TA"></textarea>
+	<br/>
 	<input type="submit" value="Send" class="submit">
 	<input type="reset" value="Cancel" class="cancel">
-</fieldset>	
-</form>
+</fieldset>
 
+</form>
+</div><!-- class="content_div" -->
+</section>
 </body>
 </html>
--- src/message/db/MessageDAO.java
+++ src/message/db/MessageDAO.java
@@ -23,7 +23,6 @@ public class MessageDAO {
 
 	//메시지보내는 작업을 하는 메소드 (DB TABLE에 저장하는 작업)
 	public int sendMessage(MessageDTO mdto) {
-		
 		Connection con=null;
 		String sql="";
 		int result=0;
@@ -38,21 +37,53 @@ public class MessageDAO {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, mdto.getFromID());
 			rs = pstmt.executeQuery();
+				
 			//id가 존재한다면
 			if(rs.next()){
+				/*받는 사람입장에서는 message_receive에 DB작업되고 동시에 보낸 사람입장에서는 message_send에 DB작업함*/
+				
+				/*--------------------------message_receive에 INSERT작업--------------------------*/
 				/*다음 작업은 접속한 사용자가 받은 메시지들의 번호 중 가장 큰 번호를 찾는 작업*/
-				sql = "select max(no) from message where fromID=?";
+				sql = "select max(no) from message_receive where fromID=?";
 				pstmt = con.prepareStatement(sql);
 				pstmt.setString(1, mdto.getFromID());
 				rs = pstmt.executeQuery();
-				//메시지 받은게 없으면 첫 번째 번호는 1로 설정 아니면 기존것의 +1
-				if(rs.next()){
+				
+				if(rs.next()){//메시지 존재하면 기존것의 +1
 					maxNum=rs.getInt(1)+1;
-				}else{
+				}else{//메시지 받은게 없으면 첫 번째 번호는 1로 설정
 					maxNum=1;}  
 				
 				/*다음은 메시지 테이블에 DB작업*/
-				sql = "insert into message(no,toID,fromID,subject,content,reg_date,read_status) values(?,?,?,?,?,?,?)";
+				sql = "insert into message_receive(no,toID,fromID,subject,content,reg_date,read_status) values(?,?,?,?,?,?,?)";
+				
+				pstmt = con.prepareStatement(sql);
+				
+				pstmt.setInt(1, maxNum); //위에서 해당 유저의 메시지함에서 가장 마지막번호+1 로 설정해둠
+				pstmt.setString(2, mdto.getToID());
+				pstmt.setString(3, mdto.getFromID());
+				pstmt.setString(4, mdto.getSubject());
+				pstmt.setString(5, mdto.getContent());
+				pstmt.setTimestamp(6, mdto.getReg_date());
+				pstmt.setInt(7, 0);
+				
+				pstmt.executeUpdate();
+				
+				/*--------------------------message_send에 INSERT작업--------------------------*/	
+				/*다음 작업은 접속한 사용자가 보낸 메시지들의 번호 중 가장 큰 번호를 찾는 작업*/
+				sql = "select max(no) from message_send where toID=?";
+
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, mdto.getToID());
+				rs = pstmt.executeQuery();
+				 
+				if(rs.next()){//메시지 존재하면 기존것의 +1
+					maxNum=rs.getInt(1)+1;
+				}else{//메시지 받은게 없으면 첫 번째 번호는 1로 설정
+					maxNum=1;}  
+				
+				/*다음은 메시지 테이블에 DB작업*/
+				sql = "insert into message_send(no,toID,fromID,subject,content,reg_date,read_status) values(?,?,?,?,?,?,?)";
 				
 				pstmt = con.prepareStatement(sql);
 				
@@ -82,8 +113,8 @@ public class MessageDAO {
 		return result; //result 반환
 	}//sendMessage()끝
 	
-	//사용자에게 온 메시지 목록list를 반환하는 메소드
-	public ArrayList<MessageDTO> getMessageList(String id){
+	//(받은, 보낸, 보관)메시지 목록list를 반환하는 메소드
+	public ArrayList<MessageDTO> getMessageList(String id, int startRow, int pageSize, String divide){
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -92,11 +123,21 @@ public class MessageDAO {
 		
 		try {
 			con = getConnection(); //DB연결
-			//해당 ID가 받은 메시지를 모두 검색
-			sql = "select * from message where fromID=?";	
-				
+			if(divide.equals("receive")){
+				//해당 ID가 받은 메시지를 모두 검색
+				sql = "select * from message_receive where fromID=? order by no limit ?,?";	
+			} else if(divide.equals("send")){
+				//해당 ID가 보낸 메시지를 모두 검색
+				sql = "select * from message_send where toID=? order by no limit ?,?";
+			} else if(divide.equals("store")){
+				//해당 ID가 보관한 메시지를 모두 검색
+				sql = "select * from message_store where storeID=? order by no limit ?,?";
+			}
+			
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
+			pstmt.setInt(2, startRow);
+			pstmt.setInt(3, pageSize);
 			rs = pstmt.executeQuery();
 			
 			while (rs.next()) {
@@ -125,8 +166,8 @@ public class MessageDAO {
 		return messageList; //해당ID가 받은 메시지목록을 담은 List리턴	
 	} //getMessageList() 끝	
 	
-	//사용자에게 온 메시지 개수반환받는 메소드
-	public int getCountMessage(String id){
+	// (받은,보낸,보관)메시지 개수반환받는 메소드
+	public int getCountMessage(String id, String divide){
 		Connection con=null;
 		String sql="";
 		int count=0;
@@ -135,9 +176,16 @@ public class MessageDAO {
 		
 		try {
 			con = getConnection();
-			//해당 ID가 받은 메시지가 총 몇개 있는가?
-			sql = "select count(*) from message where fromID=?";
-			
+			if(divide.equals("receive")){
+				//해당 ID가 받은 메시지가 총 몇개 있는가?
+				sql = "select count(*) from message_receive where fromID=?";
+			}else if(divide.equals("send")){
+				//해당 ID가 보낸 메시지가 총 몇개 있는가?
+				sql = "select count(*) from message_send where toID=?";
+			}else if(divide.equals("store")){
+				//해당 ID가 보관한 메시지가 총 몇개 있는가?
+				sql = "select count(*) from message_store where storeID=?";
+			}
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
@@ -158,24 +206,122 @@ public class MessageDAO {
 		return count; //총 메시지 수 반환
 	}//getCountMessage()끝
 	
-	public void deleteMessageCheckbox(ArrayList<Integer> list, String id){
+	//체크된 메시지들을 한번에 보관할 때 사용하는 메소드
+	public void storeMessageCheckbox(ArrayList<Integer> list, String id, String divide){
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		
+		ResultSet rs =null;
 		String sql = null;
+		int maxNum = 1;
+		ArrayList<MessageDTO> dtoList = new ArrayList<MessageDTO>(); //체크한 메시지들을 담을 list
 		
 		try {
 			con = getConnection();
+			
+			//체크한 메시지들을 다시 dtoList에 담는 과정
 			for(int i=0; i<list.size(); i++){
-				sql = "delete from message where no=? and fromID=?";
+				if(divide.equals("receive")){
+					sql = "select * from message_receive where no=? and fromID=?";
+				}else if(divide.equals("send")){
+					sql = "select * from message_send where no=? and toID=?";
+				}
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, list.get(i));
+				pstmt.setString(2, id);
+				rs = pstmt.executeQuery();
+				if(rs.next()){
+					MessageDTO mdto = new MessageDTO();
+					mdto.setNo(rs.getInt("no"));
+					mdto.setToID(rs.getString("toID"));
+					mdto.setFromID(rs.getString("fromID"));
+					mdto.setSubject(rs.getString("subject"));
+					mdto.setContent(rs.getString("content"));
+					mdto.setReg_date(rs.getTimestamp("reg_date"));
+					mdto.setRead_status(rs.getInt("read_status"));
+					mdto.setStoreID(id);
+					dtoList.add(mdto);
+				}
+			}
+			
+			//message_store에 INSERT작업
+			for(int i=0; i<list.size(); i++){
+				//message_store에 최고 번호를 찾는 작업
+				sql = "select max(no) from message_store where storeID=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, id);
+				rs = pstmt.executeQuery();
+				//메시지 받은게 없으면 첫 번째 번호는 1로 설정 아니면 기존것의 +1
+				if(rs.next()){
+					maxNum=rs.getInt(1)+1;
+				}else{
+					maxNum=1;}  
+				
+				sql = "insert into message_store(no,toID,fromID,subject,content,reg_date,read_status,storeID)"
+					+" values(?,?,?,?,?,?,?,?)";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, maxNum);
+				pstmt.setString(2, dtoList.get(i).getToID());
+				pstmt.setString(3, dtoList.get(i).getFromID());
+				pstmt.setString(4, dtoList.get(i).getSubject());
+				pstmt.setString(5, dtoList.get(i).getContent());
+				pstmt.setTimestamp(6, dtoList.get(i).getReg_date());
+				pstmt.setInt(7, dtoList.get(i).getRead_status());
+				pstmt.setString(8, dtoList.get(i).getStoreID());
+				pstmt.executeUpdate();
+			}
+			
+			//체크한 메시지들 삭제하는 작업 (받은 or 보낸 메시지함에서 보관함으로 이동하면 받은 or 보낸 메시지함에서는 삭제되어야 하므로)
+			for(int i=0; i<list.size(); i++){
+				if(divide.equals("receive")){
+					sql = "delete from message_receive where no=? and fromID=?";
+				}else if(divide.equals("send")){
+					sql = "delete from message_send where no=? and toID=?";
+				}
 				pstmt = con.prepareStatement(sql);
 				pstmt.setInt(1, list.get(i));
 				pstmt.setString(2, id);
 				
 				pstmt.executeUpdate();
 			}
-			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)con.close();
+				if (pstmt != null)pstmt.close();
+				if (rs != null)rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}//storeMessageCheckbox()끝
+	
+	//체크된 메시지들을 한번에 삭제할 때 사용하는 메소드
+	public void deleteMessageCheckbox(ArrayList<Integer> list, String id, String divide){
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			con = getConnection();
+			for(int i=0; i<list.size(); i++){
+				//구분값에 따라 삭제 쿼리문을 설정
+				if(divide.equals("receive")){
+					sql = "delete from message_receive where no=? and fromID=?";
+				}else if(divide.equals("send")){
+					sql = "delete from message_send where no=? and toID=?";
+				}else if(divide.equals("store")){
+					sql = "delete from message_store where no=? and storeID=?";
+				}
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, list.get(i));
+				pstmt.setString(2, id);
+				
+				pstmt.executeUpdate(); //DELETE
+			}	
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -186,22 +332,29 @@ public class MessageDAO {
 				e.printStackTrace();
 			}
 		}
-		
 	}//deleteMessageCheckbox()끝
 	
-	public void deleteMessageButton(int no, String id){
+	//메시지 내용에서 삭제버튼 눌렀을시 삭제를 진행하는 메소드
+	public void deleteMessageButton(int no, String id, String divide){
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		String sql = null;
 		try {
 			con = getConnection();
-				sql = "delete from message where no=? and fromID=?";
+				//구분값에 따라 삭제 쿼리문을 설정
+				if(divide.equals("receive")){
+					sql = "delete from message_receive where no=? and fromID=?";
+				} else if(divide.equals("send")){
+					sql = "delete from message_send where no=? and toID=?";
+				}else if(divide.equals("store")){
+					sql = "delete from message_store where no=? and storeID=?";
+				}
 				pstmt = con.prepareStatement(sql);
 				pstmt.setInt(1, no);
 				pstmt.setString(2, id);
 				
-				pstmt.executeUpdate();		
+				pstmt.executeUpdate();//DELETE
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -212,11 +365,10 @@ public class MessageDAO {
 				e.printStackTrace();
 			}
 		}
-		
 	}//deleteMessageButton()끝
 	
 	//넘겨받은 번호에 해당하는 메시지 정보를 담은 DTO객체를 반환하는 메소드
-	public MessageDTO getMessage(int no){
+	public MessageDTO getMessage(int no, String divide){
 		Connection con=null;
 		String sql="";
 		PreparedStatement pstmt=null;
@@ -224,11 +376,18 @@ public class MessageDAO {
 		
 		try {
 			con = getConnection();
-			sql = "select * from message where no=?";
+			//구분값에 따라 검색 쿼리문을 설정
+			if(divide.equals("receive")){
+				sql = "select * from message_receive where no=?";
+			}else if(divide.equals("send")){
+				sql = "select * from message_send where no=?";
+			}else if(divide.equals("store")){
+				sql = "select * from message_store where no=?";
+			}
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, no);
 			rs = pstmt.executeQuery();
-			
+			//검색 값이 존재한다면
 			if(rs.next()){
 				MessageDTO mdto = new MessageDTO();
 				mdto.setNo(rs.getInt("no"));
@@ -251,10 +410,10 @@ public class MessageDAO {
 				e.printStackTrace();
 			}
 		}
-		return null;
+		return null; //검색 값 없으면 null반환
 	}//getMessage()끝
 	
-	//read_status값을 1로 바꿔주어 읽은메시지로 처리
+	//read_status값을 1로 바꿔주어 읽은메시지로 처리하는 메소드
 	public void updateReadStatus(int no){
 		Connection con=null;
 		String sql="";
@@ -263,14 +422,14 @@ public class MessageDAO {
 		try {
 			con = getConnection();
 			//해당 번호의 메시지의 read_status상태가 0인것이
-			sql = "select * from message where no=? and read_status=0";
+			sql = "select * from message_receive where no=? and read_status=0";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, no);
 			rs = pstmt.executeQuery();
 			//존재한다면
 			if(rs.next()){
 				//그것의 read_status값을 1로 바꿔준다.
-				sql = "update message set read_status=1 where no=?";
+				sql = "update message_receive set read_status=1 where no=?";
 				pstmt = con.prepareStatement(sql);
 				pstmt.setInt(1, no);
 				pstmt.executeUpdate();
@@ -288,7 +447,7 @@ public class MessageDAO {
 		}
 	}//updateReadStatus()끝
 	
-	//받은 메시지 중 안읽은 메시지가 몇 개인지 반환
+	//받은 메시지 중 안 읽은 메시지가 몇 개인지 반환
 	public int getCountDontRead(String id){
 		Connection con=null;
 		String sql="";
@@ -299,7 +458,7 @@ public class MessageDAO {
 		try {
 			con = getConnection();
 			//해당 ID가 받은 메시지 중 read_status==0 즉, 안 읽은 메시지가 몇개인가??
-			sql = "select count(*) from message where fromID=? and read_status=0";
+			sql = "select count(*) from message_receive where fromID=? and read_status=0";
 			
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
